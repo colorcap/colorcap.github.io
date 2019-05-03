cea358a3b7abdfb30f6efd756700aae798a7ac51
color <capturethecolor@gmail.com>
2019-05-03 22:48:53 +0530

    update1
    publish

---
+++ js/google-map/map-active.js
@@ -0,0 +1,40 @@
+var map;
+var latlng = new google.maps.LatLng(22.253466, 73.192604);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+        saturation: -10
+            }]
+        }];
+var mapOptions = {
+    zoom: 25,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var geocoder_map = new google.maps.Geocoder();
+var address = 'Vadodara';
+geocoder_map.geocode({
+    'address': address
+}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: map.getCenter(marker)
+        });
+    } else {
+        alert("Geocode was not successful for the following reason: " + status);
+    }
+});
+var mapType = new google.maps.StyledMapType(stylez, {
+    name: "Grayscale"
+});
+map.mapTypes.set('gMap', mapType);
+map.setMapTypeId('gMap');
